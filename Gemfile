020095103c6bc09501c5babf21e94a6da6a1f30b
Viki <ramasamy.vignesh@gmail.com>
2015-07-24 07:52:53 +0530

    Added amws-s3 for heroku support

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.5.1'
 gem 'paperclip', '~> 4.2'
+gem 'aws-sdk', '~> 2'
 
 group :development, :test do
      gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
   # Required for heroku app
   # Update 'localhost' with your hostname
   config.action_mailer.default_url_options = { host: 'viki-streetfull.herokuapp.com', port: 3000 }
+
+  # This part let paperclip to upload images to aws s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
